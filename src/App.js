0ed13b4a8babbe83b659bbf33009d734264e62b7
phuongnam148 <120701564+phuongnam148@users.noreply.github.com>
2023-03-02 23:32:07 +0700

    edit 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import './App.css';
+import { Header, Footer, Navbar, Home, GioiThieu, Products } from './components';
+
+const App = () => (
+
+  <BrowserRouter>
+    <div >
+      <Header />
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/gioi-thieu" exact element={<GioiThieu />} />
+        <Route path="/Products" exact element={<Products />} />
+      </Routes>
+      <Footer />
+
     </div>
-  );
-}
+  </BrowserRouter>
+
+);
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+export { default as Navbar } from './Navbar'
+export { default as Home } from './Home'
+export { default as Products } from './Products'
+export { default as GioiThieu } from './GioiThieu'
+export { default as Header } from './blocks/Header'
+export { default as Footer } from './blocks/Footer'
+
+
+
