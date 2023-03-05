7cd04e7fece8a45043cd73690be637a376b4e6f0
phuongnam148 <120701564+phuongnam148@users.noreply.github.com>
2023-03-05 19:52:09 +0700

    edit

--- src/components/Layout/SideBarLayout.jsx
+++ src/components/Layout/SideBarLayout.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+
 import Footer from '../blocks/footer/Footer';
 import Header from '../blocks/header/Header';
 import { Alert } from 'react-bootstrap';
@@ -11,18 +9,18 @@ const SideBarLayout = ({ children }) => {
 	return (
 		<div>
 			<Header />
-			<Container>
+			<div className='container'>
 				<Alert variant='primary'>
-					<Row>
-						<Col sm={4}>
+					<div className='row'>
+						<div className='sm-4'>
 							<h1>SideBar</h1>
-						</Col>
-						<Col sm={8}>
+						</div>
+						<div className='sm-8'>
 							<h1>{children}</h1>
-						</Col>
-					</Row>
+						</div>
+					</div>
 				</Alert>
-			</Container>
+			</div>
 			<Footer />
 		</div>
 	);
