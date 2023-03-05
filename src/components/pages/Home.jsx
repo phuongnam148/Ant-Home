d6d533f5bc1f8abc7e8c5cae9398b5ea357a0599
manden520 <108379565+3TTruong@users.noreply.github.com>
2023-03-05 20:37:26 +0700

    Update Home.jsx

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import banner from '../../images/slider_1.jpg';
-
+import './scss/home.scss';
 const Home = () => {
 	return (
 		<div className='container-fluid'>
 			<img className='img-fluid' src={banner} alt='' />
 			<div>Home Component</div>
 			<div className='.container-lg'>
-				<div className=''></div>
+				<div className='cate'></div>
 			</div>
 		</div>
 	);
