d35fde938ba9557e6572b1c5edff1be4383569d1
tienbachphan <tienbackphan@gmail.com>
2023-03-11 09:21:48 +0700

    update breadcrumb

--- src/components/pages/ProductDetail.jsx
+++ src/components/pages/ProductDetail.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import './scss/product-detail.scss';
 
 const ProductDetail = () => {
-	return <div>ProductDetail</div>;
+	return (
+		// Thanh breadcrumb
+		<div className=''>
+			<ul className='breadcrumb'>
+				<li>
+					<a href='#'>Trang chủ</a>
+				</li>
+				<li>
+					<a href='#'>Cây</a>
+				</li>
+				<li>
+					<a href='#'>Cây nhỏ</a>
+				</li>
+			</ul>
+		</div>
+	);
 };
 
 export default ProductDetail;
