3243e5819afb24385d17d518ac77dbbf29cf2e4e
duongvanz99 <duonglv99qn@gmail.com>
2023-03-25 14:41:46 +0700

    up

--- src/components/admin/Sidebar.jsx
+++ src/components/admin/Sidebar.jsx
@@ -5,26 +5,58 @@ const sidebar = () => {
 	return (
 		<div className='sidebar'>
 			<div className='top'>
-				<span className='logo'>admin-ant-home</span>
+				<span className='logo'>Admin-Ant-Home</span>
 			</div>
 			<hr />
 			<div className='center'>
 				<ul>
+					<p className='tittle'>MAIN</p>
 					<li>
 						<span>Dashboard</span>
 					</li>
+					<p className='tittle'>LISTS</p>
 					<li>
-						<span>Dashboard</span>
+						<span>Users</span>
 					</li>
 					<li>
-						<span>Dashboard</span>
+						<span>Products</span>
 					</li>
 					<li>
-						<span>Dashboard</span>
+						<span>Orders</span>
+					</li>
+					<li>
+						<span>Delivery</span>
+					</li>
+					<p className='tittle'>USEFUL</p>
+					<li>
+						<span>Stats</span>
+					</li>
+					<li>
+						<span>Notifications</span>
+					</li>
+					<p className='tittle'>SERVICE</p>
+					<li>
+						<span>System Health</span>
+					</li>
+					<li>
+						<span>Logs</span>
+					</li>
+					<li>
+						<span>Settings</span>
+					</li>
+					<p className='tittle'>USER</p>
+					<li>
+						<span>Profile</span>
+					</li>
+					<li>
+						<span>Logout</span>
 					</li>
 				</ul>
 			</div>
-			<div className='bottom'>color options</div>
+			<div className='bottom'>
+				<div className='colorOption'></div>
+				<div className='colorOption'></div>
+			</div>
 		</div>
 	);
 };
