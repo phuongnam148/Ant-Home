e3b8adbf27b69bb4a70b9b539f97a69cf05b4a6c
duongvanz99 <duonglv99qn@gmail.com>
2023-04-01 00:57:32 +0700

    up

--- src/components/admin/Adminhome.jsx
+++ src/components/admin/Adminhome.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import Sidebar from '../admin/Sidebar';
 import Navbar from '../admin/Navbar';
 import './admincss/homeadmin.scss';
+import Widget from '../admin/Widget';
 const Adminhome = () => {
 	return (
 		<div className='adminhome'>
 			<Sidebar />
 			<div className='container'>
 				<Navbar />
-				Home container
+				<div className='widgets'>
+					<Widget type='user' />
+					<Widget type='order' />
+					<Widget type='earning' />
+					<Widget type='balance' />
+				</div>
 			</div>
 		</div>
 	);
