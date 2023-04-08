1bad89418eeae6817e58eb78aa2c7c89daf5176f
nlpn1408 <nlpn1481999@gmail.com>
2023-04-08 19:18:57 +0700

    .

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,9 @@ import * as bootstrap from 'bootstrap';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { CheckOut, GioiThieu, Home, Products } from './components';
 import DefaultLayout from './Layout/DefaultLayout';
-import ProductDetail from './components/pages/ProductDetail';
+// import ProductDetail from './components/pages/ProductDetail';
+import ProductDetailTest from './components/pages/ProductDetailTest';
+
 import Login from './components/pages/login/Login';
 import Pagenotfound from './components/pages/Pagenotfound';
 import Register from './components/pages/login/Register';
@@ -35,7 +37,7 @@ const router = createBrowserRouter([
 		children: [
 			{ path: '', element: <Home /> },
 			{ path: '/home', element: <Home /> },
-			{ path: '/product/:id', element: <ProductDetail /> },
+			{ path: '/product', element: <ProductDetailTest /> },
 			{ path: '/gioi-thieu', element: <GioiThieu /> },
 			{ path: '/products', element: <Products /> },
 			{ path: '/login', element: <Login /> },
