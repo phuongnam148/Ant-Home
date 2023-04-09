3ec8f417226e23f178fa8cb08d8daaed8b1ba7f9
Nguyễn Trung Khiêm <trungkhiem1412@gmail.com>
2023-04-10 00:46:13 +0700

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { store } from './store.js';
 import { Provider } from 'react-redux';
-import { AuthProvider } from './context/AuthProvider';
 import { BrowserRouter } from 'react-router-dom';
 
 // Import our custom CSS
@@ -16,9 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<BrowserRouter>
-				<AuthProvider>
-					<App />
-				</AuthProvider>
+				<App />
 			</BrowserRouter>
 		</Provider>
 	</React.StrictMode>
