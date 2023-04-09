6ee66fc3fb181efa91d8713f845292e6b125d1c1
phuongnam148 <120701564+phuongnam148@users.noreply.github.com>
2023-04-09 18:22:41 +0700

    .

---
+++ src/reducers/productReducer.js
@@ -0,0 +1,45 @@
+export const INITIAL_STATE = {
+	name_prod: '',
+	price_prod: 0,
+	CategoryChild: {
+		id_category_child: 0,
+		name_category_child: '',
+		Category: {
+			id_category: 0,
+			name_category: '',
+		},
+	},
+	Brand: {
+		name_brand: '',
+	},
+	ImgProduct: [],
+	DetailProduct: {
+		detail_prod: '',
+		description_prod: '',
+		specification_prod: '',
+		preserve_prod: '',
+	},
+};
+
+export const gigReducer = (state, action) => {
+	switch (action.type) {
+		case 'CHANGE_INPUT':
+			return {
+				...state,
+				[action.payload.name]: action.payload.value,
+			};
+
+		case 'ADD_IMAGES':
+			return {
+				...state,
+				ImgProduct: [...state.ImgProduct, action.payload],
+			};
+		case 'REMOVE_IMAGES':
+			return {
+				...state,
+				ImgProduct: state.ImgProduct.filter((image) => image !== action.payload), // giữ lại tất cả image trừ  action.payload
+			};
+		default:
+			return state;
+	}
+};
