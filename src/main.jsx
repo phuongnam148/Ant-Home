8799a40e527b60fa70038d9e512d48fae4617c06
Nguyễn Trung Khiêm <trungkhiem1412@gmail.com>
2023-04-10 17:12:11 +0700

    ok

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+const queryClient = new QueryClient();
 
 // Import our custom CSS
 import './scss/style.scss';
@@ -11,8 +13,10 @@ import * as bootstrap from 'bootstrap';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</QueryClientProvider>
 	</React.StrictMode>
 );
